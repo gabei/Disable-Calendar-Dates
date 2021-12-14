a9eb769caa433a9f8af139fa6c748f0b5839a386
gabei <gabe1792@gmail.com>
2021-12-14 17:29:42 -0600

    fixed missing parens after forEach statement

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // add CSS classes to wordpress to disable these buttons, or add them in cod ehere
 // preferably the former
 
-
 /* will need to add an event listener to the arrow buttons on the calendar page
    in order to run this check every time someone changes months! */
 const nextMonthButtons = document.querySelectorAll('.flatpickr-next-month');
@@ -10,18 +9,18 @@ nextMonthButton.addEventListener('click', disableSundays);
 const findSundays = () => {
   let calendarDates = document.querySelectorAll('.flatpickr-day ');
 
-  calendarDates.forEach((day) => { 
-    let date = new Date(day.getAttribute("aria-label"));
+  calendarDates.forEach((day) => {
+    let date = new Date(day.getAttribute('aria-label'));
     let dayOfWeek = date.getUTCDay();
-    if(dayOfWeek === 0 ){
+    if (dayOfWeek === 0) {
       disableSunday(day);
     }
-  };
-}
+  });
+};
 
 const disableSunday = (days) => {
   console.log(days);
-}
+};
 
 /**********************/
 /* RUN ONCE INITIALLY */
